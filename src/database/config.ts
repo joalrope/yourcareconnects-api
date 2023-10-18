6b87f04e36bdd14822aa599006a739dbb1147bcf
joalrope-bohiques <jrodriguez@bohiques.com>
2023-10-18 15:36:03 -0400

    refactor database connection url

--- src/database/config.ts
+++ src/database/config.ts
@@ -1,13 +1,17 @@
 import mongoose from "mongoose";
 
 export const dbConnection = async () => {
+  const user = process.env.MONGO_USER;
+  const password = process.env.MONGO_PASSWORD;
+  const cluster = process.env.MONGO_CLUSTER;
+
   try {
     await mongoose
-      .connect(String(process.env.MONGODB_URI), {
-        useNewUrlParser: true,
+      .connect(`mongodb+srv://${user}:${password}@${cluster}`, {
         useUnifiedTopology: true,
         useCreateIndex: true,
         useFindAndModify: false,
+        useNewUrlParser: true,
       })
       .then(() => console.log("connection successful...."))
       .catch((err) => console.error(err));
