192bbc85101f0546a6774e8811aa3042375b1499
joalrope-bohiques <jrodriguez@bohiques.com>
2023-11-20 00:00:23 -0400

    feat socket someone is typing

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -9,52 +9,29 @@ interface connectedUsers {
   [key: string]: newUsers;
 }
 
-export function setupSockets(server: any) {
-  const connectedUsers: connectedUsers = {};
+const connectedUsers: connectedUsers = {};
 
+export function setupSockets(server: any) {
   const io = new Server(server, {
     cors: {
       origin: "http://localhost:3000",
       methods: ["GET", "POST"],
     },
+    connectionStateRecovery: {
+      maxDisconnectionDuration: 2 * 60 * 1000,
+      skipMiddlewares: true,
+    },
   });
 
-  const clients: string[] = [];
-
   io.on("connection", (socket: Socket) => {
-    console.log(`client connecting on server with socket id:${socket.id}`);
-
-    clients.push(socket.id);
-
-    console.log(clients);
-
-    //socket.emit("requestInfo", socket.id);
-
-    /* socket.on("sendData", (data) => {
-      // Manipulate the data here
-      const { id, names, socketId } = data;
-
-      if (id && names) {
-        connectedUsers[id] = {
-          names,
-          socketId,
-        };
-
-        console.log("new user connected", { id, names, socketId: socket.id });
-
-        //io.emit("connectedUsers", connectedUsers);
-      }
-    }); */
-
     socket.on("signIn", ({ id, names, socketId }) => {
-      if (id && names) {
+      if (id && names && socketId) {
         console.log("Client signed in", { id, names, socketId });
+
         connectedUsers[id] = {
           names,
           socketId,
         };
-
-        //kkkkk
 
         io.emit("connectedUsers", connectedUsers);
       }
@@ -65,24 +42,32 @@ export function setupSockets(server: any) {
       console.log("sender: ", data, "is connected withsocket id: ", socket.id);
     });
 
+    socket.on("userIsTyping", ({ isTyping, names }) => {
+      console.log("Client is typing", { isTyping, names });
+      socket.broadcast.emit("userIsTyping", { isTyping, names });
+    });
+
     socket.on("sendMessage", (data) => {
-      console.log({ connectedUsers });
-      console.log("sendMessage", data);
-      socket.to(data.receiver.socketId).emit("receiveMessage", data);
+      const { receiverId } = data;
+
+      const socketId = receiverId ? connectedUsers[receiverId].socketId : "";
+
+      if (!socketId) {
+        socket.emit("unsentMessage", data);
+        return;
+      }
+
+      socket.to(socketId).emit("receiveMessage", data);
     });
 
     socket.on("disconnect", () => {
       console.log("Client disconnected", socket.id);
 
-      /* for (var key in connectedUsers) {
-        var objectInKey = connectedUsers[key];
-
-        if (objectInKey["socketId"] === socket.id) {
+      for (let key in connectedUsers) {
+        if (connectedUsers[key].socketId === socket.id) {
           delete connectedUsers[key];
         }
       }
-      console.log({ connectedUsers });
-      socket.broadcast.emit("connectedUsers", connectedUsers); */
     });
   });
 }
