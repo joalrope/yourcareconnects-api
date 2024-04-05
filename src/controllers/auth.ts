ad6a56d05e253f5c4fc6ba90d25afa18916c4187
joalrope-bohiques <jrodriguez@bohiques.com>
2024-04-05 13:54:13 -0400

    fix path address password restore

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -216,7 +216,7 @@ export const forgotPassword = async (req: Request, res: Response) => {
       <div>
         <p style="margin:0px;">At <span>https://yourcareconnects.com/</span> we have received a request to reset your password. To reset your password please click on this button:
         <div style="margin: 64px;text-align: center">
-          <a style="block:inline;border:1px solid #1a1a13;border-radius:5px;height:60px;padding:5px 25px;margin:120px;text-decoration:none;background:#fbd467;color:#1a1a13;font-family: arial, sans-serif;font-size: 1em;line-height:1em;white-space: nowrap" href="http://localhost:3000/auth/change-password/${init}/${code}/${data}">Reset your password</a>
+          <a style="block:inline;border:1px solid #1a1a13;border-radius:5px;height:60px;padding:5px 25px;margin:120px;text-decoration:none;background:#fbd467;color:#1a1a13;font-family: arial, sans-serif;font-size: 1em;line-height:1em;white-space: nowrap" href="https://app.yourcareconnects.com/auth/change-password/${init}/${code}/${data}">Reset your password</a>
         </div>
         
         <p>or copy and paste this link in your browser</p>
